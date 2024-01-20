17fcac313640194bcaf1c6e6852f0ed1a86e41a0
Anikó Fejes <oranjje@gmail.com>
2024-01-20 23:48:12 +0100

    ✨ Graph to track session length, doesn't print ones duh

--- omenseeker.js
+++ omenseeker.js
@@ -1,5 +1,8 @@
 const Command = require('commander').Command; 
 const MapTester = require('./mapTester.js');
+const Ervy = require('ervy');
+const scatter = Ervy.scatter;
+const fg = Ervy.fg;
 
 const program = new Command();
 
@@ -9,27 +12,60 @@ program
   .option('-r, --runs <runs>', 'number of runs to perform')
   .description('test a map')
   .action((source, options) => {
-    const map = require(`./maps/${source}.json`);
-    const runs = options.runs || 1;
-    console.log('testing map...');
-    let runSuccessCount = 0;
-    let runPageCountSum = 0;
-    let runPageCountMax = 0;
-    for (let i = 0; i < runs; i++) {
-      const mapTester = new MapTester(map, options.debug);
-      const result = mapTester.testMap();
-      if (result.state === 'success') {
-        runSuccessCount++;
-      }
-      runPageCountSum += result.pageCount;
-      if (result.pageCount > runPageCountMax) {
-        runPageCountMax = result.pageCount;
-      }
-    }
-    console.log(`success rate: ${runSuccessCount/runs * 100}% (${runSuccessCount}/${runs})`);
-    console.log(`average page count: ${runPageCountSum/runs}, max page count: ${runPageCountMax}`);
+    testMap(source, options.runs, options.debug);
   });
 
-console.log('program called');
+console.log('let\'s gooo');
+
+const testMap = (source, runs, debug) => {
+  const map = require(`./maps/${source}.json`);
+  runs = runs || 1;
+  console.log('testing map...');
+  let runSuccessCount = 0;
+  let runPageCountSum = 0;
+  let runPageCountMax = 0;
+  const runStats = [];
+  for (let i = 0; i < runs; i++) {
+    const mapTester = new MapTester(map, debug);
+    const result = mapTester.testMap();
+    if (result.state === 'success') {
+      runSuccessCount++;
+    }
+    runPageCountSum += result.pageCount;
+    if (result.pageCount > runPageCountMax) {
+      runPageCountMax = result.pageCount;
+    }
+    runStats.push(result);
+  }
+  console.log(`success rate: ${runSuccessCount/runs * 100}% (${runSuccessCount}/${runs})`);
+  console.log(`average length: ${runPageCountSum/runs} rounds, max length: ${runPageCountMax} rounds`);
+  
+  // Ervy chart
+  const OK_LENGTH_THRESHOLD = 6;
+  const chartData = {};
+  for (let i = 0; i < runStats.length; i++) {
+    chartData[runStats[i].pageCount] = chartData[runStats[i].pageCount] ? chartData[runStats[i].pageCount] + 1 : 1;
+  }
+  const scatterData = [];
+  for (const key in chartData) {
+    const length = parseInt(key, 10);
+    const count = chartData[key];
+    scatterData.push({
+      key: 'A',
+      value: [length, count],
+      style: fg(length > OK_LENGTH_THRESHOLD ? 'red' : 'green', count >= 10 ? '*' : count)
+    });
+  }
+
+  console.log(scatter(scatterData, { 
+    legendGap: 10,
+    vGap: 3,
+    zero: '+',
+    height: Math.max(...Object.values(chartData)) + 2,
+    width: runPageCountMax + 1,
+    hName: 'game length (rounds)',
+    vName: '# / ' + runs + ' runs',
+  }) + '\n')
+}
 
 program.parse();
